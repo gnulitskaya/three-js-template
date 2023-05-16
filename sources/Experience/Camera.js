cb30198b4894bd26802aa2b8dd3bf99813e5e2d2
gnulitskaya <webgnule@mail.ru>
2023-05-16 23:52:40 +0300

    add animations

--- sources/Experience/Camera.js
+++ sources/Experience/Camera.js
@@ -1,16 +1,27 @@
 import * as THREE from "three";
 import Experience from "./Experience.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { Vector3 } from 'three'
 
 export default class Camera {
+  parallax = {
+    intensity: 0.8,
+    speed: 4,
+    enabled: true
+  }
+
+  lookAtStartParameters = new Vector3()
+
   constructor() {
     this.experience = new Experience();
     this.sizes = this.experience.sizes;
     this.scene = this.experience.scene;
     this.canvas = this.experience.canvas;
+    this.time = this.experience.time;
 
     this.setInstance();
     this.setControls();
+    this.setCursor();
   }
 
   setInstance() {
@@ -22,12 +33,29 @@ export default class Camera {
     );
     this.instance.position.set(8, 0, 8);
 
-    this.scene.add(this.instance);
+    //Parallax Group
+    this.cameraParallaxGroup = new THREE.Group()
+    this.cameraParallaxGroup.add(this.instance)
+    this.scene.add(this.cameraParallaxGroup)
   }
+
   setControls() {
     this.controls = new OrbitControls(this.instance, this.canvas);
-    this.controls.enableDamping = true;
+    // rotation model
+    this.controls.enabled = false;
+
+    this.controls.enableDamping = false;
   }
+
+  // set cursor for parallax effect 
+  setCursor() {
+    this.cursor = {}
+
+    window.addEventListener('mousemove', (event) => {
+        this.cursor.x = event.clientX / this.sizes.width - 0.5
+        this.cursor.y = event.clientY / this.sizes.height - 0.5
+    })
+}
 
   resize() {
     this.instance.aspect = this.sizes.width / this.sizes.height;
@@ -35,6 +63,24 @@ export default class Camera {
   }
 
   update() {
-    this.controls.update();
+    if (this.controls)
+        this.controls.update()
+    
+    if (!this.sizes.touch && this.parallax.enabled)
+        this.updateParallax()
   }
+
+  // update parallax animation using cursor movement 
+  updateParallax() {
+    const parallaxX = this.cursor.x * this.parallax.intensity
+    const parallaxY = -this.cursor.y * this.parallax.intensity
+    const deltaTime = this.time.delta / 1000
+
+    const byX = (parallaxX - this.cameraParallaxGroup.position.x) * this.parallax.speed * deltaTime
+    const byY = (parallaxY - this.cameraParallaxGroup.position.y) * this.parallax.speed * deltaTime
+
+    //Update camera position
+    if (byX < 0.05 && byX > -0.05) this.cameraParallaxGroup.position.x += byX
+    if (byY < 0.05 && byY > -0.05) this.cameraParallaxGroup.position.y += byY
+}
 }
--- sources/script.js
+++ sources/script.js
@@ -1,4 +1,4 @@
-import "./style.scss";
+// import "./style.scss";
 import Experience from "./Experience/Experience.js";
 
 const experience = new Experience(document.getElementById("experience"));
